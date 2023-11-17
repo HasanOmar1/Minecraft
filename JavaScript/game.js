529aa9b6bdff261e11edb3becc561515b2a3a89d
HasanOmar1 <r3domar1@gmail.com>
2023-11-17 15:31:52 +0200

    added

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -4,5 +4,11 @@ import  * as draw  from "./Draw.js"
 function game(){
     draw.drawGame()
 }
-
 game()
+
+
+const resetBtn = document.querySelector('#reset-game')
+
+resetBtn.addEventListener('click' , () => {
+    window.location = window.location
+})
