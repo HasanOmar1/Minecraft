d6ea28544454420f0e7e7238ed0373efdd53a01f
HasanOmar1 <r3domar1@gmail.com>
2023-11-18 18:59:08 +0200

    added

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,15 +1,23 @@
  
 import  * as draw  from "./Draw.js"
 
+
+const backgroundMusic = new Audio('/audio/very-nice-song.mp4')
+backgroundMusic.play()
+backgroundMusic.volume = 0.08
+backgroundMusic.loop = true;
+// play game
+function game(){
+draw.drawGame()
+}
+game()
+
+
+
 // reset game
 const resetBtn = document.querySelector('#reset-game')
 resetBtn.addEventListener('click' , () => {
     localStorage.clear()
     window.location = window.location
 })
-    
-function game(){
-    draw.drawGame()
-}
-game()
 
